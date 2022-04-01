ab757774cbe09a28a682bbe80b4821a8faf925e2
nishank23 <bansalnishank4@gmail.com>
2022-04-01 12:52:34 +0530

    - second commit

---
+++ Urban_Food/app/src/main/java/com/example/urban_food/Activites/ForgetPasswordScreen/forgetpassword.java
@@ -0,0 +1,16 @@
+package com.example.urban_food.Activites.ForgetPasswordScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.urban_food.R;
+
+public class forgetpassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgetpassword);
+    }
+}
