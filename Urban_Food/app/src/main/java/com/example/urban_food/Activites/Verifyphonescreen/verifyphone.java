c310a47a26995241cf29458c408804b0c5615542
nishank23 <bansalnishank4@gmail.com>
2022-04-04 11:58:09 +0530

    - second commit

---
+++ Urban_Food/app/src/main/java/com/example/urban_food/Activites/Verifyphonescreen/verifyphone.java
@@ -0,0 +1,16 @@
+package com.example.urban_food.Activites.Verifyphonescreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.urban_food.R;
+
+public class verifyphone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verifyphone);
+    }
+}
