aa335236371392508567de28d7c21c9730cc4e9b
shan <shanpatel9116@gmail.com>
2022-04-04 12:51:02 +0530

    location custom dialog

---
+++ Urban_Food/app/src/main/java/com/example/urban_food/Activites/OtpActivity.java
@@ -0,0 +1,17 @@
+package com.example.urban_food.Activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.urban_food.databinding.ActivityOtpBinding;
+
+public class OtpActivity extends AppCompatActivity {
+    ActivityOtpBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        binding=ActivityOtpBinding.inflate(getLayoutInflater());
+        super.onCreate(savedInstanceState);
+        setContentView(binding.getRoot());
+    }
+}
