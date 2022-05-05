ba1bedb0a2788277ab1e6cb5eb771fa882a13a6e
nishank <bansalnishank4@gmail.com>
2022-05-05 13:30:10 +0530

    new bottom sheet implemented

--- Urban_Food/app/src/main/java/com/example/urban_food/Activites/Login/LoginActivityPresenter.java
+++ Urban_Food/app/src/main/java/com/example/urban_food/Activites/Login/LoginActivityPresenter.java
@@ -3,6 +3,7 @@ package com.example.urban_food.Activites.Login;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 
 import com.example.urban_food.Api.ApiClient;
 import com.example.urban_food.model.LoginModel;
