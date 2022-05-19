cd37ec731aeb1392a91d7f585edf1fd0b96ab1b5
patel-parth01 <pparthyoyo82@gmail.com>
2022-05-19 12:48:49 +0530

    past order id

---
+++ Urban_Food/app/src/main/java/com/example/urban_food/Activities/PastOrder/PastOrderIdActivity.java
@@ -0,0 +1,14 @@
+package com.example.urban_food.Activities.PastOrder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PastOrderIdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_order_id);
+    }
+}
